0e8493516fb8893554d1e4f2abc641279d1fcd35
Rasika <rasika2murali1301@gmail.com>
2024-06-24 19:37:49 -0400

    Adding additional data and formality and genre code

--- code/translate_text.py
+++ code/translate_text.py
@@ -1,43 +1,66 @@
 from googletrans import Translator, LANGUAGES
 import pandas as pd 
 import numpy as np 
+import os 
 from matplotlib import pyplot as plt 
 
 
-data = pd.read_csv(r'C:\Users\rasik\Documents\IS\1_15_final_formatted_data.csv')
-print(data.head())
-print(data.columns)
+#data1 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\1_15_final_formatted_data.csv')
+data2= pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\16_35_final_formatted_data.csv')
+#data3 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\36_150_final_formatted_data.csv')
+#data4 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\151_500_final_formatted_data.csv')
+#data5 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\501_1500_final_formatted_data.csv')
+#data6 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\1501_5000_final_formatted_data.csv')
+#data7 =pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\5001_final_formatted_data.csv')
+
+# Store the DataFrames in a list
+dataframes = [data2]
+
+# Initialize the translator
+translator = Translator()
 
 def translate_text(text, dest_lang='en'):
+    # Initialize the Translator object
     translator = Translator()
+    
     try:
-        translated = translator.translate(text, dest='en')
+        # Translate the text
+        translated = translator.translate(text, dest=dest_lang)
+        
+        # Return the translated text
+        #print(translated.text)
         return translated.text
     except Exception as e:
+        # Print the error message if any exception occurs
         print(f"Error: {e}")
         return None
 
-# langugaes = list(data['lang']) 
+# for i, df in enumerate(dataframes, start=1):
+#     translated_texts = [] 
+#     for _,row in df.iterrows(): 
+#         if row['lang'] == 'non-English' or row['lang'] == 'non-English': 
+#             translated_texts.append(translate_text(row['preprocessed rules']))
+#         else: 
+#             translated_texts.append(row['preprocessed rules'])
+#     df['translated text'] = translated_texts
+#     df.to_csv(f'{i}_translated_final.csv')
 
-# for lang in langugaes: 
-#     if lang == 'non-Englsih': 
-#         print("not en")
+#sample = data1.sample(n=50)
 translated_text = []
-for _, row in data.iterrows(): 
-    if row['lang'] != 'non-Englsih': 
-        #print(row)
+for _, row in data2.iterrows(): 
+    #print(row['preprocessed rules'])
+    if row['lang'] == 'non-Englsih' or row['lang'] == 'non-English': 
         translated_text.append(translate_text(row['preprocessed rules']))
+    else: 
+        translated_text.append(row['preprocessed rules'])
+# print(translated_text)
+# print(len(translated_text))
+data2['translated rules'] = translated_text
+data2.to_csv('16_35_final.csv')
 
 
 
+    
 
-# # Example usage
-# text_to_translate = "Hola, ¿cómo estás?"
-# translated_text = translate_text(text_to_translate)
 
-# if translated_text:
-#     print(f"Original text: {text_to_translate}")
-#     print(f"Translated text: {translated_text}")
-# else:
-#     print("Translation failed.")
 
