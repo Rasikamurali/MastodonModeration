00dbf5a04790d415c496b72b0157b2ec0dd91f23
Rasika <rasika2murali1301@gmail.com>
2024-06-25 01:48:51 -0400

    Adding new data folder with formality and genre labels and scores and analysis code

--- code/formality_genre_analysis.py
+++ code/formality_genre_analysis.py
@@ -4,16 +4,24 @@ from matplotlib import pyplot as plt
 import os 
 import seaborn as sns 
 from scipy import stats
+from collections import Counter
 
-# # Specify the path to the folder
-# folder_path = r"C:\Users\rasik\Documents\Independent Study\data\formality_genre_data"
+# Specify the path to the folder
+folder_path = r"C:\Users\rasik\Documents\Independent Study\data\formality_genre_data"
 
-# # List all files and directories in the folder
-# files = os.listdir(folder_path)
+# List all files and directories in the folder
+files = os.listdir(folder_path)
 
-# for file in files: 
-#     file_path = os.path.join(folder_path, file)
-#     df = pd.read_csv(file_path)
+instance_group = [] 
+for file in files: 
+    file_path = os.path.join(folder_path, file)
+    group_naming = (file.split('.')[0].split('_'))
+    group = group_naming[0] + 'to' + group_naming[1]
+    print(group)
+    df = pd.read_csv(file_path)
+    size = len(df)
+    instance_group.extend([group] * size)
+
 
 #splitting the dfs 
 data1 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\formality_genre_data\1_15_formality_genre.csv')
@@ -24,20 +32,19 @@ data5 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\formality_
 data6 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\formality_genre_data\1501_5000_formality_genre.csv')
 data7 =pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\formality_genre_data\5001_formality_genre.csv')
 
-pre_150 = pd.concat([data1, data2, data3])
-#print(len(pre_150))
+full_data = pd.concat([data1, data2, data3, data4, data5, data6, data7])
+full_data['instance group'] = instance_group
 
-post_150 = pd.concat([data4, data5, data6, data7])
+full_data.to_csv('complete_data.csv')
 
-def formality(pre_150, post_150): 
-    pre_150_fif = [] 
-    post_150_fif = []
-    for _, row in pre_150.iterrows(): 
-        pre_150_fif.append(len(row['formality label']))
-    for _, row in post_150.iterrows(): 
-        post_150_fif.append(len(row['formality label']))
-    return pre_150_fif, post_150_fif
+print(full_data.head())
 
-pre_150_fif_list, post_150_fif_list = formality(pre_150, post_150)
-t_statistic, p_value = stats.ttest_ind(pre_150_fif_list, post_150_fif_list)
-print(t_statistic, p_value)
+print(full_data.columns)
+instance_groups = ['1to15', '16to35', '36to150', '151to500', '501to1500', '1501to5000', '5001toformality']
+
+for group in instance_groups: 
+    filtered_df = full_data[full_data['instance group']==group]
+    #print(filtered_df.head())
+    print(group)
+    formality_labels = filtered_df['formality label']
+    print(Counter(formality_labels))
