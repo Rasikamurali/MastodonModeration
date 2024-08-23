fa3e7e45907df9534c3cbb6deb066b03f3c63c1b
Rasika <rasika2murali1301@gmail.com>
2024-08-23 18:37:03 -0400

    Tidy data and improved variable names

--- code/formal_informal.py
+++ code/formal_informal.py
@@ -4,39 +4,43 @@ from matplotlib import pyplot as plt
 import os 
 from transformers import pipeline
 
-data1 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\1_15_final.csv')
-data2= pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\16_35_final.csv')
-data3 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\36_150_final.csv')
-data4 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\151_500_final.csv')
-data5 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\501_1500_final.csv')
-data6 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\1501_5000_final.csv')
-data7 =pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\5001_final.csv')
+# data1 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\1_15_final.csv')
+# data2= pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\16_35_final.csv')
+# data3 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\36_150_final.csv')
+# data4 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\151_500_final.csv')
+# data5 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\501_1500_final.csv')
+# data6 = pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\1501_5000_final.csv')
+# data7 =pd.read_csv(r'C:\Users\rasik\Documents\Independent Study\data\translated data\5001_final.csv')
 
-
-pipe = pipeline("text-classification", model="s-nlp/xlmr_formality_classifier")
-pipe2 = pipeline("text-classification", model="classla/xlm-roberta-base-multilingual-text-genre-classifier")
+data = pd.read_csv('sampled_dataset.csv')
+print(data.head())
+formality_classifier = pipeline("text-classification", model="s-nlp/xlmr_formality_classifier")
+genre_classifier = pipeline("text-classification", model="classla/xlm-roberta-base-multilingual-text-genre-classifier")
 
 
 # Store the DataFrames in a list
-dataframes = [data1,data2, data3, data4, data5, data6, data7]
-names = ['1_15', '16_35', '36_150', '151_500', '501_1500', '1501_5000', '5001']
+# dataframes = [data1,data2, data3, data4, data5, data6, data7]
+# names = ['1_15', '16_35', '36_150', '151_500', '501_1500', '1501_5000', '5001']
 
 # Iterate over the list and print the head of each DataFrame
-for i, df in enumerate(dataframes, start=1):
-    formality_label = [] 
-    formality_score = [] 
-    genre_label = []
-    genre_score = [] 
-    print(df.head())
-    texts = df['translated rules']
-    for text in texts: 
+
+formality_label = [] 
+formality_score = [] 
+genre_label = []
+
+
+texts = data['translated rules']
+for text in texts: 
         #print(pipe(text))
-        formality_label.append(pipe(text)[0]['label'])
-        formality_score.append(pipe(text)[0]['score'])
-        genre_label.append(pipe2(text)[0]['label'])
-        genre_score.append(pipe2(text)[0]['score'])
-    df['formality label'] = formality_label
-    df['formality score'] = formality_score
-    df['genre label'] = genre_label
-    df['genre score'] = genre_label
-    df.to_csv(f'{names[i-1]}_formality_genre.csv')
+        formality_label.append(formality_classifier(text)[0]['label'])
+        formality_score.append(formality_classifier(text)[0]['score'])
+        genre_label.append(genre_classifier(text)[0]['label'])
+        
+data['formality label'] = formality_label
+data['formality score'] = formality_score
+data['genre label'] = genre_label
+
+print(data.head())
+print(data.columns)
+
+data.to_csv('formality_sampled_dataset.csv')
